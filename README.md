78ac6975d1392a63e42c10d7132a3294f1b8e2df
Shaun Nielsen <shaun.nielsen@health.nsw.gov.au>
2021-04-15 09:49:24 +1000

    First full commit to github

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# pooptracker
-Poop tracker to webpage
+# Poop tracker to webpage
+
+This repo hold code to take data from the [Poop Tracker app by Appstronaut Studios](https://play.google.com/store/apps/details?id=com.appstronautstudios.pooplog&hl=en_AU&gl=US), generate some data summaries using R, and then have those values plug into a single static webpage with various [chartJS](https://www.chartjs.org/) graphics.
+
+## Data collection and export
+
+Data is collected within the poop tracker app. This app has implicit collection of:
+
+  1. Poop time and date  
+  2. Stool type (following the 7 types in the Bristol Stool Chart)
+  3. Stool size  
+  4. Stool colour - not used here 
+  5. Notes - This is free text, and in I've used it to track place
+  
+Data is exported from the Poop Tracker app to the `./raw-data` folder.
+
+## Data analysis
+
+The script `raw-data-to-json.R` uses R to generate a number of summaries. Briefly,
+this includes:  
+
+  1. Duration of data collection
+  2. Total number of poops  
+  3. Summaries by day, hour, size, shape and place  
+  
+These are stored in a list, which is then convert to a JSON file called `poop_data.json`, and
+this is saved in trhe `./web/data` folder.
+
+## Data presentation
+
+The `./web` folder holds the necessary files for a static webpage presentation of the poop data.
+It uses:
+
+  1. A minified HTML5 boiler plate from [html5boilerplate.com](https://html5boilerplate.com/)
+  2. The W3.CSS framework from [w3schools.com](https://www.w3schools.com/W3CSs/)  
+  3. The [chartJS](https://www.chartjs.org/) JavaScript library with the [rough plugin](https://github.com/nagix/chartjs-plugin-rough) for that
+  hand-drawn look
+  4. "Indie Flower" cursive font from [fonts.google](https://fonts.google.com/specimen/Indie+Flower)
+  5. 'Custom JavaScript' code for plugging in values from the data analysis
+  
+`./web/index.html` acts as a template with placeholders for values and plots. The 'Custom JavaScript' code takes the `./web/data/poop_data.json` and inserts the values into the placeholders.
+
+The final version looks something like this:
+
+![webpage-screenshot](screenshot_webpage.PNG)
+
+
+
---
+++ raw-data-to-json.R
@@ -0,0 +1,218 @@
+# Raw poop tracker csv data wrangling and output to JSON for webpage
+#
+#
+
+library(tidyverse)
+
+# functions ---
+english_date <- function(date){
+  
+  day <-
+    lubridate::day(date) %>% 
+    scales::ordinal()
+  
+  month_year <- 
+    format(date, '%B %Y')
+  
+  paste(day, month_year)
+  
+}
+
+get_often_values <- function(x, var, threshold_prob, format_hour = F){
+  
+  collapse <- function(x){
+    if (length(x) > 1) {
+      paste(x, collapse = ' to ')
+    } else {
+      x
+    }
+  }
+  
+  var = rlang::enquo(var)
+  
+  x_selected <-
+    x %>% 
+    filter(prob > threshold_prob) %>% 
+    arrange(!!var)
+  
+  if (nrow(x_selected) > 1 ){
+    x_selected = slice(x_selected, c(1, n()))
+  }
+  
+  x_vars <- 
+    x_selected %>% 
+    pull(!!var)
+  
+  if (format_hour) {
+    x_vars <- sprintf('%02d%s', x_vars, '00')
+  }
+  
+  x_vars %>% 
+    collapse()
+}
+
+
+# data ---
+poops <- readr::read_csv('raw-data/export_20210414.csv')
+
+# * n ----
+min_date_english <- english_date(min(poops$date))
+max_date_english <- english_date(max(poops$date))
+
+n_days <-
+  poops$date %>% 
+  as.Date() %>% 
+  range() %>% 
+  diff() %>% 
+  as.numeric()
+
+n_months <- 
+  round(n_days / 30.5, digits = 0)
+
+n_years <-
+  round(n_days / 365.25, digits = 2)
+
+n_poops <- nrow(poops)
+
+
+
+# * plot data ----
+# ** Poops per day
+daily <-
+  poops %>% 
+  mutate(day = as.Date(date)) %>% 
+  count(day, name = 'poops') %>% 
+  complete(day = seq.Date(from = min(day),
+                          to = max(day),
+                          by = 'day'),
+           fill = list(poops = 0)) %>% 
+  count(poops) %>% 
+  mutate(prob = n / sum(n) * 100,
+         prob = round(prob, 1))
+
+likely_n_poop <-
+  daily %>% 
+  slice(which.max(n)) %>% 
+  pull(poops) %>% 
+  as.character() %>% 
+  switch('0' = 'less than once',
+         '1' = 'once',
+         '2' = 'twice',
+         '3' = 'three or more times')
+
+poop_rate <- round(n_poops / n_days, 1)
+
+# ** Poops per hour
+hourly <-
+  poops %>% 
+  mutate(hour = lubridate::hour(date)) %>% 
+  count(hour) %>% 
+  complete(hour = seq(1:23),
+           fill = list(n = 0)) %>% 
+  arrange(hour) %>% 
+  mutate(prob = n / sum(n) * 100,
+         prob = round(prob, 1))
+
+likely_poop_time <-
+  hourly %>% 
+  slice(which.max(n)) %>% 
+  pull(hour) %>% 
+  sprintf('%02d%s', ., '00')
+
+often_poop_time <-
+  hourly %>% 
+  get_often_values(var = hour, threshold_prob = 10, format_hour = T)
+
+# ** Poop size
+size <-
+  poops %>% 
+  mutate(quantity = sub('Very Large', 'Very-Large', quantity),
+         quantity = factor(quantity, 
+                           levels = c('Small', 'Small-Medium',
+                                      'Medium', 'Medium-Large', 
+                                      'Large', 'Very-Large'))) %>% 
+  count(quantity) %>% 
+  mutate(prob = n / sum(n) * 100,
+         prob = round(prob, 1))
+
+often_poop_size <-
+  size %>% 
+  get_often_values(var = quantity, threshold_prob = 30)
+
+# ** Poop type
+type_map <- 
+  tibble(type = paste('Type', 1:7),
+         label = c('Hard lumps', 'Lumpy sausage', 'Cracked sausage',
+                   'Smooth sausage', 'Soft blobs', 'Mushy', 'Liquid'))
+
+type <-
+  poops %>% 
+  mutate(type = factor(type, 
+                       levels = type_map$type,
+                       labels = type_map$label)) %>% 
+  count(type) %>% 
+  complete(type,
+           fill = list(n = 0)) %>% 
+  mutate(prob = n / sum(n) * 100,
+         prob = round(prob, 1))
+
+often_poop_type <-
+  type %>% 
+  get_often_values(var = type, threshold_prob = 50)
+
+
+# ** Poop places
+place <-
+  poops %>% 
+  filter(as.Date(date) > '2020-05-01' &
+           !is.na(notes)) %>% 
+  mutate(place = case_when(
+    grepl('^[Hh]ome', notes) ~ 'Home',
+    grepl('^[Ww]ork', notes) ~ 'Work',
+    grepl('[Mm]other|[Nn]icole', notes) ~ 'Family',
+    TRUE ~ 'Other'),
+    place = factor(place, levels = c('Home', 'Work', 'Family', 'Other'))) %>% 
+  count(place) %>% 
+  mutate(prob = n / sum(n) * 100,
+         prob = round(prob, 1))
+
+often_poop_place <-
+  place %>% 
+  get_often_values(var = place, threshold_prob = 50)
+
+# toJson ----
+
+# * break labels for chartJS
+size <-
+  size %>% 
+  mutate(quantity = 
+           as.character(quantity) %>%
+           strsplit(split = '-'))
+type <-
+  type %>% 
+  mutate(type = 
+           as.character(type) %>%
+           strsplit(split = ' '))
+
+# * values for text
+list(start_date = min_date_english,
+     end_date = max_date_english,
+     n_days = paste(n_days, 'days'),
+     n_months = paste(n_months, 'months'),
+     n_years = paste(n_years, 'years'),
+     n_poops = n_poops,
+     likely_n_poop = likely_n_poop,
+     poop_rate = paste(poop_rate, 'poops per day'),
+     likely_poop_time = likely_poop_time,
+     often_poop_time = often_poop_time,
+     often_poop_size = often_poop_size,
+     often_poop_type = often_poop_type,
+     often_poop_place = often_poop_place,
+     daily = daily,
+     hourly = hourly,
+     size = size,
+     type = type,
+     place = place) %>% 
+  jsonlite::toJSON(dataframe = 'columns') %>% 
+  writeLines('web/data/poop_data.json')
+
